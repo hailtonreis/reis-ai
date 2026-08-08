9502e7bc9b55a5ce3308780d5edf4f1f1ebf49ae
hailton.reis <hailton.reis@gmail.com>
2026-08-08 02:06:21 -0300

    configura CORS para frontend de producao

--- app/main.py
+++ app/main.py
@@ -15,6 +15,7 @@ app.add_middleware(
      CORSMiddleware,
      allow_origins=[
          "http://localhost:4200"
+         "https://hailtonreis.tech"
     ],
         allow_credentials=True,
         allow_methods=["*"],
